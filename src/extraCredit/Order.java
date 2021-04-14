7520059dbcd0ea00818b22e6b9a2e4880c9f4e9c
thenewcreation <galatafir@yahoo.com>
2021-04-14 17:47:39 -0500

    initial

---
+++ src/extraCredit/Order.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Order implements Cloneable { 
+	
+	String ordernumber ;
+
+	boolean prepaid ;
+
+	double orderprice ;
+
+	Date d1 ;
+	
+	List <OrderLine> list;
+	
+	@Override
+	protected Object  clone() throws CloneNotSupportedException {
+		Order o = (Order) super.clone();
+		o.d1 = (Date) d1.clone();
+				
+		List<OrderLine> list1 = new ArrayList<OrderLine>();
+		
+		for(OrderLine l: list) {
+			list1.add((OrderLine)l.clone());
+		}
+		o.list = list1;
+		return o;
+	}
+	
+	void m1(Order o2) {
+		
+	}
+}
+
---
+++ src/extraCredit/OrderLine.java
@@ -0,0 +1,11 @@
+package extraCredit;
+
+public class OrderLine implements Cloneable{
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return (OrderLine) super.clone();
+	}
+
+}
